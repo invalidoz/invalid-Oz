a300d33a8b92c338f0d42d45e434725c125b059c
Lökis <invalidoz@users.noreply.github.com>
2015-05-15 21:38:47 +0200

    Update header.php

--- header.php
+++ header.php
@@ -1,7 +1,30 @@
 <!doctype html>
-<html>
+<html lang="sv">
 <head>
-  <meta charset="utf-8">
-  <title><?= $title ?></title>
-</head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="The fact and the truth about the clan INVALIDOZ">
+    <meta name="author" content="Lökis and Grottis">
+    <link rel="icon" href="../../favicon.ico">
+        <title><?= $title ?></title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/cover.css" rel="stylesheet">
+	
+		<link rel="stylesheet" href="css/sidebar-left.css">
+
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
+	<link href='http://fonts.googleapis.com/css?family=Cookie' rel='stylesheet' type='text/css'>
+
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
 <body>
